40cbeee2481995a9c9db00cc51ebc208aa7671f1
Haiying Weng <48968041+haiyingweng@users.noreply.github.com>
2020-02-06 15:08:34 -0500

    Setup Google signin (#4)
    * Add google sign in
    
    * Remove Pods
    
    * Add Pods to .gitignore
    
    * Address PR comments
    
    * Add logo image view
    
    * Address PR comments

---
+++ CoffeeChat/Controller/LoginViewController.swift
@@ -0,0 +1,83 @@
+//
+//  LoginViewController.swift
+//  CoffeeChat
+//
+//  Created by HAIYING WENG on 1/29/20.
+//  Copyright © 2020 cuappdev. All rights reserved.
+//
+
+import GoogleSignIn
+import SnapKit
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    private let loginButton = GIDSignInButton()
+    private let logoImageView = UIImageView()
+
+    private let logoSize = CGSize(width: 150, height: 150)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().presentingViewController = self
+
+        loginButton.style = .wide
+        view.addSubview(loginButton)
+
+        logoImageView.layer.cornerRadius = logoSize.width/2
+        logoImageView.backgroundColor = .gray
+        view.addSubview(logoImageView)
+
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        loginButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(logoImageView.snp.bottom).offset(40)
+        }
+
+        logoImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-100)
+            make.size.equalTo(logoSize)
+        }
+    }
+
+}
+
+extension LoginViewController: GIDSignInDelegate {
+
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+                print("The user has not signed in before or they have since signed out.")
+            } else {
+                print("\(error.localizedDescription)")
+            }
+            return
+        }
+
+        print(user.userID)
+        print(user.authentication.idToken)
+        print(user.profile.name)
+        print(user.profile.givenName)
+
+        if let email = user.profile.email, !(email.contains("@cornell.edu")) {
+            GIDSignIn.sharedInstance().signOut()
+            let alertController = UIAlertController(title: Constants.Alerts.LoginFailure.title, message: Constants.Alerts.LoginFailure.message, preferredStyle: .alert)
+            let action = UIAlertAction(title: Constants.Alerts.LoginFailure.action, style: .cancel, handler: nil)
+            alertController.addAction(action)
+            present(alertController, animated: true, completion: nil)
+            return
+        }
+
+        let homeVC = HomeViewController()
+        homeVC.modalPresentationStyle = .fullScreen
+        present(homeVC, animated: true, completion: nil)
+    }
+
+}
--- CoffeeChat/SceneDelegate.swift
+++ CoffeeChat/SceneDelegate.swift
@@ -6,18 +6,32 @@
 //  Copyright © 2020 cuappdev. All rights reserved.
 //
 
+import GoogleSignIn
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = scene as? UIWindowScene else { return }
+
+        let window = UIWindow(windowScene: scene)
+        guard let signIn = GIDSignIn.sharedInstance() else {
+            window.rootViewController = LoginViewController()
+            return
+        }
+        if signIn.hasPreviousSignIn() {
+            signIn.restorePreviousSignIn()
+            window.rootViewController = HomeViewController()
+        } else {
+            window.rootViewController = LoginViewController()
+        }
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
