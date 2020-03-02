3d6ff8611b4adab001d2ea7974bbbccc3c158ae7
lucyxubroad <llx2@cornell.edu>
2020-03-02 12:52:18 -0500

    Remove noMatchVC from scenedelegate

--- CoffeeChat/SceneDelegate.swift
+++ CoffeeChat/SceneDelegate.swift
@@ -31,9 +31,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             // Onboard user if they haven't done so yet, otherwise bring to home.
             let onboardingCompleted = userDefaults.bool(forKey: Constants.UserDefaults.onboardingCompletion)
             let homeVC = HomeViewController()
-            let noMatchVC = NoMatchViewController()
+//            let noMatchVC = NoMatchViewController()
             let onboardingVC = OnboardingPageViewController(transitionStyle: UIPageViewController.TransitionStyle.scroll, navigationOrientation: UIPageViewController.NavigationOrientation.horizontal)
-            let rootVC = onboardingCompleted ? noMatchVC : onboardingVC
+            let rootVC = onboardingCompleted ? homeVC : onboardingVC
             let navigationController = UINavigationController(rootViewController: rootVC)
             window.rootViewController = navigationController
         } else {
