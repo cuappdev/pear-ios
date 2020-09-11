fc342c39e9c4cf16a97edbf705bb99400f03d07c
Phillip O'Reggio <pnoreggio@gmail.com>
2020-09-11 02:07:26 -0400

    Clean up FadeTableView

--- CoffeeChat/View/FadeTableView.swift
+++ CoffeeChat/View/FadeTableView.swift
@@ -8,19 +8,24 @@
 
 import UIKit
 
-/// UITableView that fades its top and bottom to blend in with its background
+/// UITableView that fades its top and bottom based on `fadeColor` to make the edges blend in with the background
 class FadeTableView: UIView {
 
     let tableView = UITableView(frame: .zero, style: .plain)
 
     private let fadeColor: UIColor
     private let transparentColor = UIColor(displayP3Red: 1, green: 1, blue: 1, alpha: 0)
+
     private let topFadeView = UIView()
     private let bottomFadeView = UIView()
 
     init(fadeColor: UIColor) {
         self.fadeColor = fadeColor
         super.init(frame: .zero)
+
+        addSubview(tableView)
+        addSubview(topFadeView)
+        addSubview(bottomFadeView)
         setupConstraints()
     }
 
@@ -36,24 +41,19 @@ class FadeTableView: UIView {
     private func applyFadeGradient() {
         let fadeColors = [fadeColor.cgColor, transparentColor.cgColor]
 
-        let topLayer = CAGradientLayer()
-        topLayer.frame = topFadeView.bounds
-        topLayer.colors = fadeColors
-        topLayer.locations = [0.0, 1.0]
-        topFadeView.layer.insertSublayer(topLayer, at: 0)
+        addGradientToView(colors: fadeColors, view: topFadeView)
+        addGradientToView(colors: fadeColors.reversed(), view: bottomFadeView)
+    }
 
-        let bottomLayer = CAGradientLayer()
-        bottomLayer.frame = bottomFadeView.bounds
-        bottomLayer.colors = fadeColors.reversed()
-        bottomLayer.locations = [0.0, 1.0]
-        bottomFadeView.layer.insertSublayer(bottomLayer, at: 0)
+    private func addGradientToView(colors: [CGColor], view: UIView) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = colors
+        gradientLayer.locations = [0.0, 1.0]
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
     private func setupConstraints() {
-        addSubview(tableView)
-        addSubview(topFadeView)
-        addSubview(bottomFadeView)
-
         let topFadeHeight: CGFloat = 10
         let bottomFadeHeight: CGFloat = 26
 
