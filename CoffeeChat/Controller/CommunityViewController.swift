72983f7d9ec7a1fe061455a0890b04774f188fdb
Amy Huang <ach243@cornell.edu>
2021-01-26 10:30:26 -0600

    fix visual bugs

--- CoffeeChat/Controller/CommunityViewController.swift
+++ CoffeeChat/Controller/CommunityViewController.swift
@@ -18,7 +18,7 @@ class CommunityViewController: UIViewController {
     private var users: [CommunityUser] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-
+ 
         view.backgroundColor = .backgroundLightGreen
 
         searchBar.delegate = self
@@ -36,11 +36,6 @@ class CommunityViewController: UIViewController {
         searchBar.layer.shadowRadius = 4
         searchBar.layer.shadowOffset = CGSize(width: 0, height: 2)
         view.addSubview(searchBar)
-
-        // TODO - delete after taking screenshots for pull request
-        let dummy = CommunityUser(groups: ["group", "group", "group", "group", "group", "group", "group", "group", "group"], firstName: "Amy", googleID: "592", graduationYear: "2023", hometown: "Ithaca", interests: ["interest", "interest", "interest", "interest", "interest", "interest", "interest", "interest", "interest", "interest", "interest", "interest", "interest"], lastName: "Huang", major: "Co", matches: [], netID: "ac", profilePictureURL: "", pronouns: "she/hers", facebook: "", instagram: "")
-        let dummy2 = CommunityUser(groups: ["group", "group", "group", "group", "group", "group", "group", "group", "group"], firstName: "Amy", googleID: "592", graduationYear: "2023", hometown: "Ithaca", interests: ["interest", "interest", "interest", "interest", "interest"], lastName: "Huang", major: "Co", matches: [], netID: "ac", profilePictureURL: "", pronouns: "she/hers", facebook: "", instagram: "")
-        users = [dummy, dummy, dummy2, dummy, dummy2]
 
         communityTableView.delegate = self
         communityTableView.dataSource = self
